04539a847c7ec91c95e05f0324fc94a6facac388
Roman Kushnarenko <sromku@gmail.com>
2016-01-09 14:11:12 +0200

    Update stream message model

--- stream.go
+++ stream.go
@@ -3,17 +3,21 @@ import (
 	"bufio"
 	"encoding/json"
 	"log"
+	"time"
 )
 
+// Initialize stream
 func (room *Room) Stream() *Stream {
 	return &Stream{
 		Room: room,
+		Url: GITTER_STREAM_API + "rooms/" + room.Id + "/chatMessages",
 		GitterMessage: make(chan GitterMessage),
 	}
 }
 
+// Start streaming api and listen to incoming messages
 func (gitter *Gitter) Listen(stream *Stream) {
-	res, _ := gitter.getResponse(GITTER_STREAM_API + "rooms/" + stream.Room.Id + "/chatMessages")
+	res, _ := gitter.getResponse(stream.Url)
 	reader := bufio.NewReader(res.Body)
 	var gitterMessage GitterMessage
 	for {
@@ -28,10 +32,24 @@ func (gitter *Gitter) Listen(stream *Stream) {
 }
 
 type GitterMessage struct {
+
+	// message id
 	Id string `json:"id"`
+
+	// message body
+	Text string `json:"text"`
+
+	// sent time
+	Sent time.Time `json:"sent"`
+
+	// from user
+	From User `json:"fromUser"`
+
 }
 
+// Definition of stream
 type Stream struct {
 	Room          *Room
+	Url           string
 	GitterMessage chan GitterMessage
 }
